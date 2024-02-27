4d735e54e56d1cdfc24b1288fa6b72b7295ea72e
Jurijs Kovzels <yuri.kovzel@gmail.com>
2024-02-27 16:23:53 +0100

    some refacoring

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,29 @@
 class Index {
 	tokens: { [key: string]: Token } = Object.create(null);
 
+	build = function build(this: Index, content: Array<string> | string){
+		const tokens = this.tokens;
+		let lastSeenIndex: {[key: string]: number} = {};
+		let tokenSrt = content[0];
+		let previous = new Token(tokenSrt, 0);
+		previous.positions.set(0, 0);
+		lastSeenIndex[content[0]] = 0; 
+		tokens[content[0]] = previous;
+	
+		for (let i = 1; i < content.length; i++) {
+			tokenSrt = content[i].toLowerCase();
+			let token = tokens[tokenSrt] || new Token(tokenSrt, i);
+			//glyph.count++;
+			lastSeenIndex[tokenSrt] !== undefined && (token.positions.set(lastSeenIndex[tokenSrt], i));
+			lastSeenIndex[tokenSrt] = i;
+			token.positions.set(i, -1);
+			tokens[tokenSrt] = token;
+			//previous.followers[char] = glyph;
+			previous = token;
+		}
+		return this;
+	}
+
 	getPositions = function getPositions(this: Index, query: string): number[]{
 		let positions = new Array<number>();
 		if(!query){
@@ -60,46 +83,19 @@ class Token {
 
 
 
-function buildIndex(){
-	console.log("up and running");
-	const index = new Index();
-
-	console.time("get_content");
-	let content = document.body.textContent || "";
-	//let text = "abc ab";
-	console.timeEnd("get_content");
-
-	const tokens = index.tokens;
-	let lastSeenIndex: {[key: string]: number} = {};
-	console.time("index");
-	let tokenSrt = content[0];
-	let previous = new Token(tokenSrt, 0);
-	previous.positions.set(0, 0);
-	lastSeenIndex[content[0]] = 0; 
-	tokens[content[0]] = previous;
-
-	for (let i = 1; i < content.length; i++) {
-		tokenSrt = content[i].toLowerCase();
-		let token = tokens[tokenSrt] || new Token(tokenSrt, i);
-		//glyph.count++;
-		lastSeenIndex[tokenSrt] !== undefined && (token.positions.set(lastSeenIndex[tokenSrt], i));
-		lastSeenIndex[tokenSrt] = i;
-		token.positions.set(i, -1);
-		tokens[tokenSrt] = token;
-		//previous.followers[char] = glyph;
-		previous = token;
-	}
-	console.timeEnd("index");
-
-	return index;
-}
-
 
 function test(){
 	
 
 
-	let index = buildIndex();
+	console.log("up and running");
+	console.time("get_content");
+	let content = document.body.textContent || "";
+	console.timeEnd("get_content");
+	console.time("build_index");
+	const index = new Index().build(content);
+	console.timeEnd("build_index");
+
 
 	window.contentIndex = index;
 
